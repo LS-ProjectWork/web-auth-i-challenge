c0a19a2d5dd0b39a68fdc99ef6e3a5bbfa6645ce
Darien Payton <darien.payton71@gmail.com>
2019-05-06 16:48:55 -0400

    created endpoints for authentication and authorization

--- index.js
+++ index.js
@@ -27,6 +27,49 @@ server.post('/api/register', (req, res) => {
     })
 });
 
+server.post('/api/login', (req, res) => {
+    const {username, password} = req.body
+
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        if(user && bycrypt.compareSync(password, user.username)) {
+            res.status(201).send('Logged in')
+        } else {
+            res.status(401).send('You shall not pass')
+        }
+    })
+    .catch(() => {
+        res.status(500).send('System could verify user')
+    })
+})
+
+server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(201).json(users)
+    })
+    .catch(err => res.json(err))
+})
+
+function restricted(req, res, next) {
+    const {username, password} = req.headers
+
+    if(username && password) {
+        Users.findBy({username})
+        .first()
+        .then(user => {
+            if(user && bycrypt.compareSync(password, user.username)) {
+                next()
+            } else {
+                res.status(401).send('You cannot pass')
+            }
+        })
+    } else {
+        res.status(401).send('Please provide credentials')
+    }
+}
+
 server.listen(3600, () => {
     console.log('Server is fired up on port 6000!')
 })
