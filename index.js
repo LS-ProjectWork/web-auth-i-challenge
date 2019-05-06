7b54026aafc7fca75b9ee14014b56b23a6ed5540
Darien Payton <darien.payton71@gmail.com>
2019-05-06 09:25:25 -0400

    created server and register endpoint

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const knex = require('knex');
+const helmet = require('helmet');
+const bcrypt = require('bycrptjs');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    console.log(`It's working`)
+});
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    user.password = bcrypt.hashSync(user.password, 10)
+
+    db
+    .insert(user)
+    .into('users')
+    .then(saved => {
+        res.status(201).json(saved)
+    })
+});
+
+server.listen(3600, () => {
+    console.log('Server is fired up on port 6000!')
+})
